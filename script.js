e41f7f10eca3b3b2053960b7c841f373f02518d0
CandiaGu <candi37@gmail.com>
2020-02-09 00:07:03 -0500

    added randomization

---
+++ script.js
@@ -0,0 +1,27 @@
+const fonduHeaders = {
+	'health':"Your personalized healthy relationship training.",
+	'fun':"Your personalized healthy relationship advisor."
+}
+
+const fonduDescriptions = {
+	'health':"Trustworthy, easy-to-use, and fun, Fondu trains you to strengthen your existing relationship or future relationships.",
+	'fun':"Trustworthy, easy-to-use, and fun, Fondu helps you to improve your existing relationship or future relationships.",
+}
+
+const randIntLink = ['health','fun']
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * Math.floor(max));
+}
+
+function randomizeDesc(){
+	let rand_int = getRandomInt(2); //choose between 0 and 1
+	let fondu_head = fonduHeaders[randIntLink[rand_int]];
+	let fondu_desc = fonduDescriptions[randIntLink[rand_int]];
+
+	document.getElementById("fondu-header").innerHTML = fondu_head;
+	document.getElementById("fondu-desc").innerHTML = fondu_desc;
+
+	document.getElementById('beta-button').onclick = function() { gtag('event', 'click', { 'event_label': randIntLink[rand_int]});};
+
+}
